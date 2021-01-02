00123e96df71f64a6ddd6a9f8ff0accd7c9d1ff8
Morgen Halle <47124777+mhalle0@users.noreply.github.com>
2021-01-02 16:59:38 -0600

    Added substring column to csv, linted JS file and prepared to implement substring titles

--- final_project.js
+++ final_project.js
@@ -14,24 +14,25 @@ function printEach(row)
 {
   console.log(row);
 }
-//Creates a tooltip to display name and data for each game
+// Creates a tooltip to display name and data for each game
 var tip = d3.tip()
-		.attr("class","d3-tip")
-		.offset([-5,0])
-		.html(function (d){
-			properties = d.target.__data__;
-			gameName = properties.Game;
-			num = properties.NumPurchased;
-			hrs = properties.HoursPlayed;
-			total = properties.TotalHours;
-			if (Math.round(hrs) >= 1)
-			     return `${gameName}</br>${Math.round(hrs)} average hours played</br>${num} purchased
-           </br>${Math.round(total)} total hours played`;
-      else
-           return `${gameName}</br> < 1 average hours played</br>${num} purchased
-           </br> < 1 total hours played`;
+  .attr("class","d3-tip")
+	.offset([-5,0])
+	.html(function (d){
+	  properties = d.target.__data__;
+		gameName = properties.Game;
+		num = properties.NumPurchased;
+		hrs = properties.HoursPlayed;
+		total = properties.TotalHours;
+		if (Math.round(hrs) >= 1)
+			return `${gameName}</br>${Math.round(hrs)} average hours played</br>${num} purchased
+      </br>${Math.round(total)} total hours played`;
+    else
+      return `${gameName}</br> < 1 average hours played</br>${num} purchased
+      </br> < 1 total hours played`;
 		});
-//Logarithmic scale for coloring the bars 
+
+// Logarithmic scale for coloring the bars 
 var logScale = d3.scaleLog().domain([minhrs, maxhrs]);
 var logColorScale = d3.scaleSequential(function(d){return d3.interpolateGreens(logScale(d));});
 var seqColorScale = d3.scaleSequential(d3.interpolateGreens).domain([minhrs, maxhrs]);
@@ -41,103 +42,109 @@ const svg = d3.select("#vizArea")
               .append("svg")
               .attr("width", width)
               .attr("height", height);
-//calling the tooltip
+
+// calling the tooltip
 svg.call(tip);
 
 // Get data
 data = d3.csv("steam-200k-cleaned.csv")
-            .then(function (data) {
-              data.forEach(function (d) {
-                d.HoursPlayed = +d.HoursPlayed;
-              });
-      // Filter data by average hours played and total copies purchased
-       var holder = {};
-       var count = {};
-       var pCount = {};
-       data.forEach(function(elem) {
-         // Excludes all null elements
-         if (elem.HoursPlayed != -1)
-         {
-           if (holder.hasOwnProperty(elem.Game))
-           {
-             holder[elem.Game] = holder[elem.Game] + elem.HoursPlayed;
-             count[elem.Game] = count[elem.Game] + 1;
-           }
-           else
-           {
-              holder[elem.Game] = elem.HoursPlayed;
-              count[elem.Game] = 1;
-           }
-         }
+         .then(function (data) {
+            data.forEach(function (d) {
+              d.HoursPlayed = +d.HoursPlayed;
+            });
+  // Filter data by average hours played and total copies purchased
+  var holder = {};
+  var count = {};
+  var pCount = {};
+  var gameSubstring = {};
+  data.forEach(function(elem) {
+    // Excludes all null elements
+    if (elem.HoursPlayed != -1)
+    {
+      if (holder.hasOwnProperty(elem.Game))
+      {
+        holder[elem.Game] = holder[elem.Game] + elem.HoursPlayed;
+        count[elem.Game] = count[elem.Game] + 1;
+      }
+      else
+      {
+        holder[elem.Game] = elem.HoursPlayed;
+        count[elem.Game] = 1;
+      }
+    }
 
-         if ((elem.Action).localeCompare("purchase"))
-         {
-           if (pCount.hasOwnProperty(elem.Game))
-           {
-             pCount[elem.Game] = pCount[elem.Game] + 1;
-           }
-           else
-           {
-              pCount[elem.Game] = 1;
-           }
-         }
-       });
-       for (var prop in holder)
-       {
+    if ((elem.Action).localeCompare("purchase"))
+    {
+      if (pCount.hasOwnProperty(elem.Game))
+      {
+        pCount[elem.Game] = pCount[elem.Game] + 1;
+      }
+      else
+      {
+        pCount[elem.Game] = 1;
+      }
+    }
+  });
 
-	hrs = holder[prop];
-	if(hrs > maxhrs){
-		maxhrs = hrs;
-	}
+  for (var prop in holder)
+  {
+	  hrs = holder[prop];
+    if(hrs > maxhrs)
+    {
+		  maxhrs = hrs;
+	  }
+	  dataByGame.push({Game: prop, NumPurchased: pCount[prop], HoursPlayed: hrs / count[prop], TotalHours: hrs, GameAbbr: gameSubstring});
+  }
 
-	 dataByGame.push({Game: prop, NumPurchased: pCount[prop], HoursPlayed: hrs / count[prop], TotalHours: hrs});
-       }
+  purchaseCount = dataByGame.sort((a, b) => b.NumPurchased - a.NumPurchased);
 
-       purchaseCount = dataByGame.sort((a, b) => b.NumPurchased - a.NumPurchased);
+  // Test to make sure correct values are being printed
+  // purchaseCount.forEach(printEach);
+  // totalPlayed.forEach(printEach);
+  // overallPlaytime.forEach(printEach);
+  // console.log(purchaseCount.length);
+  console.log(dataByGame);
 
-       // Test to make sure correct values are being printed
-         //purchaseCount.forEach(printEach);
-         //totalPlayed.forEach(printEach);
-         //overallPlaytime.forEach(printEach);
-         //console.log(purchaseCount.length);
-
-       updateGraph(dataByGame, 1);
-
+  updateGraph(dataByGame, 1);
 });
-//function uses the total hours and the logarithmic scale to get a green for the bar
-function getBarColor(d){
-	hrs = d.TotalHours;
+
+// function uses the total hours and the logarithmic scale to get a green for the bar
+function getBarColor(d)
+{
+  hrs = d.TotalHours;
 	return logColorScale(hrs);
 }
+
 // Using ints (dataKey) to identify which dataset is being used for now. Probably a better way to do this.
 // 0 is for average hours played dataset
 // 1 is for total copies purchased dataset
-//Function that actually draws the visualization after each change
+// Function that actually draws the visualization after each change
 function updateGraph(newData, dataKey)
 {
   var maXvalue = null;
   if (dataKey == 0)
-      maXvalue = 1300;
+  {
+    maXvalue = 1300;
+  }
   else
-      maXvalue = 5000;
+  {
+    maXvalue = 5000;
+  }
 
-  //set x and y axes
+  // set x and y axes
   var yAxis = d3.scaleBand()
-            .range([0, (newData.length + 1) * 28])
-            // Set paddingInner to change space between the bars
-            .paddingInner(0.25)
-            .domain(newData.map(function(d) {
-              if(d.Game.length < 42) {
-                return (d.Game);
-              }
-              else {
-                return ((d.Game.substring(0, 42) + "..."));
-              }
-            }))
+                .range([0, (newData.length + 1) * 28])
+                // Set paddingInner to change space between the bars
+                .paddingInner(0.25)
+                .domain(newData.map(function(d) 
+                {
+                  return (d.Game);
+                }
+                ));
 
   var xAxis = d3.scaleLinear()
-            .range([margin.left, width - margin.right])
-            .domain([0, maXvalue]);
+                .range([margin.left, width - margin.right])
+                .domain([0, maXvalue]);
 
   // appends background
   svg.append("rect")
@@ -147,8 +154,11 @@ function updateGraph(newData, dataKey)
 
   // change header text
   if (dataKey == 0)
+  {
     d3.select("h4").text("Games by Average Hours Played")
-  else {
+  }
+  else 
+  {
     d3.select("h4").text("Games by Total Copies Purchased")
   }
 
@@ -168,43 +178,46 @@ function updateGraph(newData, dataKey)
      .style("font-size", "14px");
 
   // create the bars for bargraph
-   svg.selectAll(".bar")
+  svg.selectAll(".bar")
      .data(newData)
      .enter()
      .append("g")
      .append("rect")
      .attr("transform", "translate(" + (margin.left + 70) + ", " + 50 + ")")
      .attr("class", "bar")
-     .attr("y", function(d) {
-      if((d.Game).length < 42) 
+     .attr("y", function(d) 
       {
         return (yAxis(d.Game));
-      }
-      else
-      {
-        return ((yAxis(d.Game.substring(0, 42) + "...")));
-      }
-    })
-       
+      })
      .attr("height", yAxis.bandwidth())
      .attr("fill", getBarColor)
      .attr("x", 0)
-     .attr("width", function(d) {if (dataKey == 0)
-                                 {
-                                   // Set minimum bar size if the data is too small
-                                   if ((xAxis(d.HoursPlayed) - margin.left) < 5)
-                                      return 5;
-                                   else
-                                      return xAxis(d.HoursPlayed) - margin.left;
-                                 }
-                                 else
-                                 {
-                                     if ((xAxis(d.NumPurchased) - margin.left) < 5)
-                                        return 5;
-                                     else
-                                        return xAxis(d.NumPurchased) - margin.left;
-                                 }
-                               })
+     .attr("width", function(d) 
+      {
+        if (dataKey == 0)
+        {
+          // Set minimum bar size if the data is too small
+          if ((xAxis(d.HoursPlayed) - margin.left) < 5)
+          {
+            return 5;
+          }
+          else
+          {
+            return xAxis(d.HoursPlayed) - margin.left;
+          }
+        }
+        else
+        {
+          if ((xAxis(d.NumPurchased) - margin.left) < 5)
+          {
+            return 5;
+          }
+          else
+          {
+            return xAxis(d.NumPurchased) - margin.left;
+          }
+        }
+        })
      .attr("stroke", "black")
      .on("mouseover",tip.show)
      .on("mouseout",tip.hide);
@@ -214,74 +227,81 @@ function updateGraph(newData, dataKey)
 var displayingAvg = false;
 function changeGraph()
 {
-      svg.selectAll("*").remove();
-      if (displayingAvg)
-      {
-          updateGraph(dataByGame, 1);
-          displayingAvg = false;
-      }
-      else
-      {
-          updateGraph(dataByGame, 0);
-          displayingAvg = true;
-      }
+  svg.selectAll("*").remove();
+  if (displayingAvg)
+  {
+    updateGraph(dataByGame, 1);
+    displayingAvg = false;
+  }
+  else
+  {
+    updateGraph(dataByGame, 0);
+    displayingAvg = true;
+  }
 }
 
-//sorts data highest to lowest
+// sorts data highest to lowest
 function sortHighest()
 {
-      svg.selectAll("*").remove();
-      if (displayingAvg)
-      {
-	dataByGame = dataByGame.sort((a,b) => b.HoursPlayed - a.HoursPlayed);
-        updateGraph(dataByGame, 0);
-      }
-      else
-      {
-	dataByGame = dataByGame.sort((a,b) => b.NumPurchased - a.NumPurchased);
-        updateGraph(dataByGame, 1);
-      }
+  svg.selectAll("*").remove();
+  if (displayingAvg)
+  {
+	  dataByGame = dataByGame.sort((a,b) => b.HoursPlayed - a.HoursPlayed);
+    updateGraph(dataByGame, 0);
+  }
+  else
+  {
+	  dataByGame = dataByGame.sort((a,b) => b.NumPurchased - a.NumPurchased);
+    updateGraph(dataByGame, 1);
+  }
 }
-//sorts data lowest to highest
+
+// sorts data lowest to highest
 function sortLowest()
 {
-      svg.selectAll("*").remove();
-      if (displayingAvg)
-      {
-	dataByGame = dataByGame.sort((a,b) => a.HoursPlayed - b.HoursPlayed);
-        updateGraph(dataByGame, 0);
-      }
-      else
-      {
-        dataByGame = dataByGame.sort((a,b) => a.NumPurchased - b.NumPurchased);
-        updateGraph(dataByGame, 1);
-      }
+  svg.selectAll("*").remove();
+  if (displayingAvg)
+  {
+	  dataByGame = dataByGame.sort((a,b) => a.HoursPlayed - b.HoursPlayed);
+    updateGraph(dataByGame, 0);
+  }
+  else
+  {
+    dataByGame = dataByGame.sort((a,b) => a.NumPurchased - b.NumPurchased);
+    updateGraph(dataByGame, 1);
+  }
 }
 
 // Sorts the data alphabetically from 'A'-'Z'. Not case sensitive.
 function sortAlphabet()
 {
-      svg.selectAll("*").remove();
-      dataByGame = dataByGame.sort(function(a, b)
-      {
-        var game1 = a.Game.toLowerCase();
-        var game2 = b.Game.toLowerCase();
-        if (game1 > game2)
-          return 1;
-        if (game2 > game1)
-          return -1;
-        else
-          return 0;
-      });
+  svg.selectAll("*").remove();
+  dataByGame = dataByGame.sort(function(a, b)
+  {
+    var game1 = a.Game.toLowerCase();
+    var game2 = b.Game.toLowerCase();
+    if (game1 > game2)
+    {
+      return 1;
+    }
+    if (game2 > game1)
+    {
+      return -1;
+    }
+    else
+    {
+      return 0;
+    }
+  });
 
-      if (displayingAvg)
-      {
-        updateGraph(dataByGame, 0);
-      }
-      else
-      {
-        updateGraph(dataByGame, 1);
-      }
+  if (displayingAvg)
+  {
+    updateGraph(dataByGame, 0);
+  }
+  else
+  {
+    updateGraph(dataByGame, 1);
+  }
 }
 
 //end of file
